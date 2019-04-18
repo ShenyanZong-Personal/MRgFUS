1bc5d5beeafcc379bc484ad31d9cc562d1f4c516
ShenyanZong-Personal <zongjsnt@aliyun.com>
2019-04-18 18:29:46 -0400

    Update codes

--- OCM/example/demo1WT.m
+++ OCM/example/demo1WT.m
@@ -29,4 +29,10 @@ hold on; plot( fs(n_us/2+1:end),abs( freq_us_data_s2( (n_us/2+1):end ) ) );
 us_data_s1_spec = spectrogram(us_data_s1,100,50,2048);
 us_data_s2_spec = spectrogram(us_data_s2,100,50,2048);
 
+[n_tfa_freq_s1,n_tfa_time_s1] = size(us_data_s1_spec); 
+fs_tfa = linspace(0,freq_sample/2,n_tfa_freq_s1);
+Ts_tfa = linspace( ((1/freq_sample)*100)/2,T_duration*(1e-3)-((1/freq_sample)*100)/2,n_tfa_time_s1 );
+
+figure; imagesc([min(Ts_tfa) max(Ts_tfa)],[min(fs_tfa) max(fs_tfa)],abs(us_data_s1_spec));
+colormap('Gray');
 
