0d3dca364cc1f680e0526ddc8ae17ea33e1a0b7d
ShenyanZong-Personal <zongjsnt@aliyun.com>
2019-06-25 19:02:50 -0400

    Update ocm_thermo.m

--- OCM/OCM_thermo/ocm_thermo.m
+++ OCM/OCM_thermo/ocm_thermo.m
@@ -8,13 +8,14 @@ if ocm_file == 0
     fprintf('no ocm data selected ...\n');
 else
     if ocm_format == 1
+        
         fprintf('read ocm data in *.bin file ...\n');
-        
         [us_ocm,us_ts1,us_ts2,us_nEl] = load_OCMdata([ocm_path ocm_file],-1);
-        
         us_t_stamps = us_ts2 - us_ts2(1);
         
         figure(1);
         imagesc(us_ocm); colormap('Gray');
     end
 end
+
+[img_file,img_path] = uigetfile({'*.dat'},'Reconstructed Images Selection');
