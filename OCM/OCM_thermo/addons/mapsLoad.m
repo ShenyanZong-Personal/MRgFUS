13231ede7a581f030366addbfd3d801a1fa07c59
ShenyanZong-Personal <zongjsnt@aliyun.com>
2019-07-08 19:17:16 -0400

    Create mapsLoad.m

---
+++ OCM/OCM_thermo/addons/mapsLoad.m
@@ -0,0 +1,15 @@
+% This file is to load Siemens MR maps which have been stored in *.mat %
+% 
+% Edit Histories:
+% 07-08-2019 /Shenyan/ Create this file by Shenyan.
+
+clear;
+
+[maps_file,maps_path] = uigetfile({'*.mat'},'Mat Data Selection');
+if maps_file == 0
+    fprintf('no *.mat file selected ...\n');
+else
+    fprintf('load maps in *.mat file ...\n');
+    DS          = load([maps_path maps_file]);
+    maps_obj_m  = DS.IM_recon;
+end
